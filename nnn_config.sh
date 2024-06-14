cb4f9834f67db3b4e103a07d3fe5878c21e362b4
Tri Pham <techcaotri@gmail.com>
2024-06-14 15:05:42 +0700

    Add "s:my_fz_open_with" to search for files and select action options

--- nnn_config.sh
+++ nnn_config.sh
@@ -1,5 +1,5 @@
 # nnn configurations
-export NNN_PLUG='*:fzplug;c:my_copy_path;C:my_fz_cmd;d:dragdrop;e:!code-insiders "$nnn"*;E:!code-insiders "$(dirname "$nnn")"*;f:my_fj_cmd;F:fzcd;j:my_z_cmd;i:my_fzf_image_preview;I:imgview;l:my_lfrun;o:my_open_with;O:my_open_with_options;p:preview-tui;Q:my_nnn_quitall;t:preview-tabbed;v:vidthumb;x:!ark -ba "$nnn" 2>/dev/null 1>/dev/null & *;X:!chmod +x $nnn;z:my_zi_cmd;'
+export NNN_PLUG='*:fzplug;c:my_copy_path;C:my_fz_cmd;d:dragdrop;e:!code-insiders "$nnn"*;E:!code-insiders "$(dirname "$nnn")"*;f:my_fj_cmd;F:fzcd;j:my_z_cmd;i:my_fzf_image_preview;I:imgview;l:my_lfrun;o:my_open_with;O:my_open_with_options;p:preview-tui;Q:my_nnn_quitall;s:my_fz_open_with;t:preview-tabbed;v:vidthumb;x:!ark -ba "$nnn" 2>/dev/null 1>/dev/null & *;X:!chmod +x $nnn;z:my_zi_cmd;'
 export NNN_FCOLORS='c1e2272e006033f7c6d6abc4'
 alias nnn=nnn_left
 alias nnn_left='/home/tripham/bin/nnn -e -a -o -r -R -i -d -H -P p -s left -S -f'
